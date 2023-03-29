03d7045ff6352b4eeed733396736ee309fdab523
seancampbell3161 <sean.campbell3161@gmail.com>
2023-03-28 23:36:10 -0500

    add some style, breadcrumbs, loading indicator

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -13,7 +14,10 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private shopService: ShopService,
-    private activatedRoute: ActivatedRoute) {}
+    private activatedRoute: ActivatedRoute,
+    private bc: BreadcrumbService) {
+      this.bc.set('@productDetails', ' ');
+    }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -23,7 +27,10 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
 
     if (id) this.shopService.getProduct(+id).subscribe({
-      next: (product) => this.product = product,
+      next: (product) => {
+        this.product = product;
+        this.bc.set('@productDetails', product.name);
+      },
       error: (error) => console.log(error)
     });
   }
