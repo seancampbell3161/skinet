24c387de430d03589a4cb88550f9daf89e87201f
seancampbell3161 <sean.campbell3161@gmail.com>
2023-04-02 14:16:18 -0500

    Seed users

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Infrastructure.Identity;
+﻿using Core.Entities.Identity;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
@@ -11,6 +13,18 @@ namespace API.Extensions
             {
                 opt.UseSqlite(config.GetConnectionString("IdentityConnection"));
             });
+
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                // Add identity options here
+            })
+            .AddEntityFrameworkStores<AppIdentityDbContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+
+            services.AddAuthorization();
+
             return services;
         }
     }
