8222121e4a246888151435da4f470b93bdb33d8d
seancampbell3161 <sean.campbell3161@gmail.com>
2023-04-08 17:51:43 -0500

    create checkout components, implement address and delivery methods

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, ReplaySubject, map, of } from 'rxjs';
+import { ReplaySubject, map, of } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 import { User } from '../shared/models/user';
 
--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { DeliveryMethod } from '../shared/models/deliveryMethod';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getDeliveryMethods() {
+    return this.http.get<DeliveryMethod[]>(this.baseUrl + 'orders/deliveryMethods').pipe(
+      map(dm => {
+        return dm.sort((a, b) => b.price - a.price);
+      })
+    );
+  }
 }
